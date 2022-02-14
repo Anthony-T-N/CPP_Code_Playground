7f3c9b59126b045b4a08260b64fb4ab50dc8a528
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-02-14 22:13:53 +1030

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<int> digitize(unsigned long n)
+{
+    std::string s = std::to_string(n);
+    std::vector<int> new_vector = {};
+    for (int i = 0; i <= s.size() - 1; i++)
+    {
+        std::cout << "Adding: " << s[i] << "\n";
+        new_vector.push_back((int)s[i]);
+    }
+    std::cout << new_vector.size() << "\n";
+    std::cout << new_vector[1] << "\n";
+    for (int i = 0; i <= new_vector.size() - 1; i++)
+    {
+        std::cout << "\n";
+        std::cout << i << "\n";
+        if ((i + 1) <= new_vector.size() - 1)
+        {
+            if (new_vector[i] < new_vector[i + 1])
+            {
+                std::swap(new_vector[i], new_vector[i + 1]);
+            }
+        }
+        for (int i = 0; i <= new_vector.size() - 1; i++)
+        {
+            std::cout << new_vector[i] << " + ";
+        }
+    }
+    std::cout << "\n";
+    std::cout << "Result" << "\n";
+    for (int i = 0; i <= new_vector.size() - 1; i++)
+    {
+        std::cout << new_vector[i] << " + ";
+    }
+    return new_vector;
+}
+
+int main()
+{
+    std::cout << "START" << "\n";
+    digitize(35231);
+}
