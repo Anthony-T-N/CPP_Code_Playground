6c9fba3bbe35148b904a8fc322bca2c349411b7a
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-02-18 21:01:00 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,11 +18,40 @@ std::vector<int> digitize(unsigned long n)
     return vector_final;
 }
 
-int main()
+int summation(int num)
 {
-    std::cout << "START" << "\n";
-    digitize(35231);
+    int sum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
 }
 
-// Commit test
-// gpg Verification test
+#include <vector>
+#include <utility>
+std::pair<int, int> rowWeights(const std::vector <int>& weights)
+{
+    int team_one = 0;
+    int team_two = 0;
+    for (int i = 0; i <= weights.size(); i++)
+    {
+        std::cout << "#: " << weights[i] << "\n";
+        if (i % 2 == 0)
+        {
+            team_one += weights[i];
+        }
+        else
+        {
+            team_two += weights[i];
+        }
+    }
+    std::cout << team_one << "\n";
+    std::cout << team_two << "\n";
+    return std::make_pair(1, 2);
+}
+
+int main()
+{
+    std::cout << "BEGIN TESTING" << "\n";
+}
