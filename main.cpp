bd71478afec955e03250a16e7273aee670d15276
Anthony <53560209+Anthony-T-N@users.noreply.github.com>
2022-05-27 22:58:11 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#include <vector>
+
+int sum(std::vector<int> nums) 
+{
+  int sum = 0;
+  for (unsigned long i = 0; i <= nums.size() - 1; i++)
+  {
+    std::cout << nums[i] << "\n";
+    sum += nums[i];
+  }
+  return sum;
+}
+
 std::vector<int> sortArray(std::vector<int> array)
 {
   if (array.empty())
