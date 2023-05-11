aafc01a30ddd376e41b8407a515b67eead9f527b
Anthony <53560209+Anthony-T-N@users.noreply.github.com>
2023-05-11 18:12:51 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,7 +41,6 @@ bool s(const std::vector<std::string>& words)
       std::cout << item << " ";
       temp_vector_two.push_back(item);
     }
-    std::cout << "\n" << "Loop" << "\n";
     int match_cout = 0;
     for (int i = 0; i <= temp_vector_two.size() - 1; i++)
     {
@@ -60,109 +59,6 @@ bool s(const std::vector<std::string>& words)
     temp_vector_two.clear();
   } 
   while(std::next_permutation(short_vector.begin(), short_vector.end()));
-  
-  std::cout << "nnnn" << "\n";
-  
-  while (true)
-  {
-    temp_vector.push_back(copied_short_vector[stage]);
-    std::cout << "START: " << copied_short_vector[stage] << "\n";
-    copied_short_vector.erase(copied_short_vector.begin() + stage);
-    int reset = 0;
-    bool second_fail = false;
-    for (int i = 0; i <= copied_short_vector.size() - 1; i++)
-    {
-      std::cout << "\n";
-      std::cout << "i: " << i << " " << copied_short_vector.size() - 1 << "\n";
-      std::cout << "Remaining: ";
-      for (int y = 0; y <= copied_short_vector.size() - 1; y++)
-      {
-        if (copied_short_vector[i] == copied_short_vector[y])
-        {
-          std::cout << "[" << copied_short_vector[y] << "]" << " | ";
-        }
-        else
-        {
-          std::cout << copied_short_vector[y] << " | ";
-        }
-      }
-      std::cout << " S:" << copied_short_vector.size() - 1;
-      std::cout << "\n" << "Current: ";
-      for (int z = 0; z <= temp_vector.size() - 1; z++)
-      {
-        std::cout << temp_vector[z] << " | ";
-      }
-      std::cout << "S:" << temp_vector.size() - 1;
-      std::cout << "\n";
-
-      std::cout << "if (" << temp_vector[ace].back() << "==" << copied_short_vector[i][0] << ")" << "\n";
-      if (temp_vector[ace].back() == copied_short_vector[i][0])
-      {
-        temp_vector.push_back(copied_short_vector[i]);
-        copied_short_vector.erase(copied_short_vector.begin() + i);
-        std::cout << "i: " << i << "\n";
-        if (i - 1 <= 0)
-        {
-          i = 0;
-        }
-        else
-        {
-          i--;
-        }
-        ace++;
-      }
-      if (temp_vector.size() == short_vector.size())
-      {
-        std::cout << "Size: " << temp_vector.size() << " == " << short_vector.size() << "\n";
-        return true;
-      }
-      std::cout << i << " " << copied_short_vector.size() - 1 << "\n";
-      if (i >= copied_short_vector.size() - 1)
-      {
-        if (reset == 2)
-        {
-          break;
-        }
-        std::cout << "Insert: " << reset << "\n";
-        std::cout << "i=-1" << "\n";
-        i = -1;
-        reset++;
-      }
-      if (copied_short_vector.size() == 1)
-      {
-        std::cout << "if (copied_short_vector.size() == 1)" << "\n";
-        if (second_fail == true)
-        {
-          break;
-        }
-        second_fail = true;
-      }
-      /*
-      else if (temp_vector[ace].back() == copied_short_vector[i][0])
-      {
-        temp_vector.push_back(copied_short_vector[i]);
-        copied_short_vector.erase(copied_short_vector.begin() + i);
-        i--;
-        ace++;
-      }
-      */
-    }
-    reset = 0;
-    std::cout << "\n" << "NEW" << "\n";
-    stage++;
-    ace = 0;
-    copied_short_vector = short_vector;
-    temp_vector.clear();
-    if (stage == copied_short_vector.size() - 1)
-    {
-      break;
-    }
-  }
-  
-  for (const auto& letters : letter_map) 
-  {
-    std::cout << "K: " << letters.first << " V: " << letters.second << "\n";
-  }
   return false;
 }
 
